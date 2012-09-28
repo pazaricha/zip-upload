f25717324725caa1a3d0b7df6f6e302cf9bae285
Paz Aricha <paz@pazaricha.com>
2012-09-28 12:46:58 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZipUpload::Application.config.secret_token = '7c8c8c2f057cd14343a0f846acfc289b5608299ed8c0229600adb4b5b77e9d6e78fabf5491f01fcc33d895e043c948570f6989b52db52dbbcd776ab45c1c5dfb'
